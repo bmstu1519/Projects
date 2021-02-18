894fdca191095446fb1b0915b99cbf826f6c9e03
Михалев Егор <egor.mikhalev@inside.glowbyte.com>
2021-02-18 20:13:53 +0300

    modify parseTableCSV.sh

--- bash/parseTableCSV.sh
+++ bash/parseTableCSV.sh
@@ -5,7 +5,6 @@ file_output="test.txt"
 ddl=
 sql_attrs=
 tb_check=
-tb_schema=
 {
 read 
 while IFS=";" read system shema table_name atr impala_types 
@@ -14,16 +13,13 @@ if [[ $tb_check !=  $table_name && ! -z "$tb_check" ]]
 then
 	ddl="CREATE TABLE IF NOT EXISTS rrp_evr_$shema.$tb_check(${sql_attrs::-1});"
 	echo "$ddl" >> "$file_output"
-	tb_schema=$shema
 	sql_attrs=
 fi
 tb_check=$table_name
 sql_attrs+="$atr String"$'\n'","
 echo "$tb_check || $atr"
-
 done }< "$file_source"
 
-ddl="CREATE TABLE IF NOT EXISTS rrp_evr_$tb_schema.$tb_check(${sql_attrs::-1});"
 echo "$ddl" >> "$file_output"
 #echo "$tb_schema $tb_check "
 
